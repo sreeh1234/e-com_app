374475f01183456899aa4dacdea3b80901c15bdf
sreehari k <162795824+sreeh1234@users.noreply.github.com>
2024-11-13 17:49:55 +0530

    e com "add product"

--- e_com/app/urls.py
+++ e_com/app/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('',views.e_com_login),
     path('shop_home',views.shop_home),
-    path('logout',views.e_com_logout)
+    path('logout',views.e_com_logout),
+    path('add product',views.e_com_addpro),
+    
 ]
--- e_com/app/views.py
+++ e_com/app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
-
+from django.contrib import messages
+from .models import product
 # Create your views here.
 
 def e_com_login(req):
@@ -12,6 +13,7 @@ def e_com_login(req):
             login(req,shop)
             return redirect(shop_home)
         else:
+            messages.warning(req,'invalid username or password')
             return redirect(e_com_login)
     else:
         return render(req,'login.html')
@@ -21,5 +23,25 @@ def e_com_logout(req):
     logout(req)
     return redirect(e_com_login)
 
+def e_com_addpro(req):
+    return render(req,'shop/add.html')
+
 def shop_home(req):
-    return render(req,'shop/home.html')
+    products=product.objects.all()
+    return render(req,'shop/home.html',{'products':products})
+
+
+# std=[{'roll_no':'1','name':'sanju','age':25},{'roll_no':'2','name':'anju','age':23}]
+def add(req):
+    if req.method=='POST':
+        pid=req.POST['pid']
+        name=req.POST['name']
+        dis=req.POST['dis']
+        price=req.POST['price']
+        offer_price=req.POST['offer_price']
+        stock=req.POST['stock']
+        std.append({'pid':pid,'name':name,'dis':dis})
+        print(std)
+        return redirect(add)
+    else:
+        return render(req,'add_std.html',{'std':std})  
