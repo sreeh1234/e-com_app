f19d8a8aa14d8fc5d8da425f787585ba0c8fe218
sreeh1234 <sreehariapzzz@gmail.com>
2025-01-16 17:41:12 +0530

    e_com mysql database creation

--- e_com/e_com/settings.py
+++ e_com/e_com/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'e_com.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+      'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'admin',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST':'127.0.0.1',
+        'PORT':'3306',
     }
 }
 
